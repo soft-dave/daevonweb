ab2639e6f6e28edc7a2e692c2949e67a8f955cd4
Dave JC <davejc99@gmail.com>
2018-03-30 15:07:03 +0530

    set next and previous arrow link to dynamically

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,11 @@
 class ProjectsController < ApplicationController
 	def index
     @portfolios = Portfolio.all
-    # @portfolio = Portfolio.find(params[:id])
   end
 
   def show
-    # @portfolios = Portfolio.published.order('created_at')
     @portfolio = Portfolio.find(params[:id])
+    @previous = Portfolio.where("id < ?", params[:id]).order(:id).last   
+    @next = Portfolio.where("id > ?", params[:id]).order(:id).first
   end
 end
